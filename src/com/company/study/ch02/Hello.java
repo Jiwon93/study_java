555a82b89efabca79d506d8fd9d5e86348d44a03
Jiwon93 <ls_sjw93@naver.com>
2022-07-26 16:14:49 +0900

    java첫날

--- src/com/company/study/ch02/Hello.java
+++ src/com/company/study/ch02/Hello.java
@@ -15,5 +15,56 @@ public class Hello {
 		int with = 100;
 		String name = "신지원";
 		
+		//상품메인
+		String productMain = "메인표지";
+		String productStar = "별점";
+		Double productGrade = 5.0;
+		
+		
+		
+		//상단바
+		String productDetail = "상품 설명";
+		String price = "가격 정보";
+		String refund = "취소 환불";
+		String review = "후기";
+		
+		//구매칸
+		int priceBasic = 990000;
+		int priceBest = 2970000;
+		int pricePro = 10000000;
+		
+		String basic = "Basic";
+		String best = "Best";
+		String pro = "Professional";
+		
+		
+		/////////////////////////////////////////////////////////
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은: " + name);
+		
+		System.out.println(abc);
+		System.out.println(xyz);
+		
+		System.out.println(with);
+		
+		System.out.println(productMain);
+		System.out.println(productStar);
+		System.out.println(productGrade);
+		
+		System.out.println(productDetail);
+		System.out.println(price);
+		System.out.println(refund);
+		System.out.println(review);
+		
+		System.out.println(priceBasic);
+		System.out.println(priceBest);
+		System.out.println(pricePro);
+		
+		System.out.println(basic);
+		System.out.println(best);
+		System.out.println(pro);
+		
+		
 	}//main() end
 }//class end

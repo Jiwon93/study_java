57f0a37078936f860063b22d604b28693d84a60a
Jiwon93 <ls_sjw93@naver.com>
2022-07-26 15:44:39 +0900

    java

---
+++ src/com/company/study/ch02/Hello.java
@@ -0,0 +1,17 @@
+package com.company.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+		// 데이터타입 변수명 = 값;
+		// 데이터타입 변수명
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+
+	}//main() end
+}//class end

844b190d3f11f70972314a46e1a483f8d335da55
Jiwon93 <ls_sjw93@naver.com>
2022-07-29 15:29:24 +0900

    int

---
+++ src/com/company/study/ch02/IntExample.java
@@ -0,0 +1,13 @@
+package com.company.study.ch02;
+
+public class IntExample {         //파일명과 class명은 동일해야함
+								  //각 단어 첫글자는 대문자로(카멜표기법)
+	public static void main(String[] args) {
+		
+		int var1 = 10;
+		
+		System.out.println(var1);
+
+	}
+
+}

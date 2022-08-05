2e104e4c75cb89a7ded320d3d3c63822f6bc9826
Jiwon93 <ls_sjw93@naver.com>
2022-08-05 11:23:01 +0900

    java

---
+++ src/com/company/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,31 @@
+package com.company.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		
+		// +=
+		
+		int result = 0;
+		
+		result += 10;
+		// result = result + 10;
+		
+		System.out.println("result: " + result);
+		
+		// -=
+		
+		result -= 5;
+		// result = result - 5;
+		
+		System.out.println("result: " + result);
+		
+		// *=
+		
+		result *= 30;
+		// result = result * 30;
+		
+		System.out.println("result: " + result);
+		
+	}
+}

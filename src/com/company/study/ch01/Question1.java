1a814edf02a589ba28affdb0809dd65beaad1270
Jiwon93 <ls_sjw93@naver.com>
2022-08-15 17:42:58 +0900

    java

---
+++ src/com/company/study/ch01/Question1.java
@@ -0,0 +1,18 @@
+package com.company.study.ch01;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+
+		// 1부터 10까지 더하는 프로그램 작성
+		
+		// 2단 부터 9단 까지 출력하는 프로그램 작성
+		// (조건. 6줄 이상 되지 않게)
+		
+		
+		// 1~100 사이의 랜덤 정수 2개를 발생시켜
+		// 두 수 사이의 합을 구하라
+	}
+
+}

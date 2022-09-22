68a62048053abde2f867f086bf52133a43010c9b
Jiwon93 <ls_sjw93@naver.com>
2022-09-22 16:41:46 +0900

    commit

--- src/com/company/study/ch10/first/TryCatchFinallyRuntimeExceptionExample.java
+++ src/com/company/study/ch10/first/TryCatchFinallyRuntimeExceptionExample.java
@@ -4,18 +4,18 @@ public class TryCatchFinallyRuntimeExceptionExample {
 
 	public static void main(String[] args) {
 		
-		String data1 = null;
-		String data2 = null;
-		try {
-			data1 = args[0];
-			data2 = args[1];
-		} catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("실행 매개값의 수가 부족합니다.");
-			System.out.println("[실행 방법]");
-			System.out.println("java TryCatchFinallyRuntimeExceptionExample  num1  num2");
-			return;
-		} 
-		
+		String data1 = "1";
+		String data2 = "2";
+//		try {
+//			data1 = args[0];
+//			data2 = args[1];
+//		} catch(ArrayIndexOutOfBoundsException e) {
+//			System.out.println("실행 매개값의 수가 부족합니다.");
+//			System.out.println("[실행 방법]");
+//			System.out.println("java TryCatchFinallyRuntimeExceptionExample  num1  num2");
+//			return;
+//		} 
+//		
 		try { 
 			int value1 = Integer.parseInt(data1);
 			int value2 = Integer.parseInt(data2);
